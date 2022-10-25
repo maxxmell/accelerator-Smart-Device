12e80e0c8fc48d96089ec86fb8d80211d51cf39c
Sergey <Maxxmell@yandex.ru>
2022-10-25 10:43:37 +0300

    Fixed accordion

--- source/js/modules/accordion.js
+++ source/js/modules/accordion.js
@@ -1,52 +1,57 @@
 function renderAccordion(targetClass, maxViewport) {
   let viewportWidth = window.innerWidth || document.documentElement.clientWidth;
   let accordion = document.querySelector(`.${targetClass}`);
-  hideAll();
+  let headerElement = document.querySelectorAll(`.${targetClass}__header`);
+  let blockElement = document.querySelectorAll(`.${targetClass}__block`);
 
-  if (viewportWidth < maxViewport) {
-    try {
-      accordion.addEventListener('click', change);
-    } catch (e) {
-      reportError(e);
-    }
-  }
+  hideAll();
 
   window.addEventListener('resize', function () {
     viewportWidth = window.innerWidth || document.documentElement.clientWidth;
     try {
-      if (viewportWidth < maxViewport) {
-        accordion.addEventListener('click', change);
-      } else {
-        accordion.removeEventListener('click', change);
+      if (accordion) {
+        if (viewportWidth < maxViewport) {
+          accordion.addEventListener('click', change);
+        } else {
+          accordion.removeEventListener('click', change);
+        }
       }
     } catch (e) {
       reportError(e);
     }
   }, false);
 
+  if (viewportWidth < maxViewport) {
+    window.dispatchEvent(new Event('resize'));
+  }
+
   function change(event) {
-    let targetElement = event.target;
-    if (!(targetElement.classList.contains(`${targetClass}__header`))) {
-      return;
-    }
-    if (targetElement.classList.contains(`${targetClass}__header--selected`)) {
-      hideAll();
-    } else {
-      hideAll();
-      targetElement.classList.add(`${targetClass}__header--selected`);
-      showBlock(targetElement.nextElementSibling);
+    try {
+      let targetElement = event.target;
+      if (!(targetElement.classList.contains(`${targetClass}__header`))) {
+        return;
+      }
+      if (targetElement.classList.contains(`${targetClass}__header--selected`)) {
+        hideAll();
+      } else {
+        hideAll();
+        targetElement.classList.add(`${targetClass}__header--selected`);
+        showBlock(targetElement.nextElementSibling);
+      }
+    } catch (e) {
+      reportError(e);
     }
   }
 
   function hideAll() {
-    let headerElement = accordion.querySelectorAll(`.${targetClass}__header`);
-    let blockElement = accordion.querySelectorAll(`.${targetClass}__block`);
     try {
-      for (let i = 0; i < headerElement.length; i++) {
-        headerElement[i].classList.remove(`${targetClass}__header--selected`);
-      }
-      for (let i = 0; i < blockElement.length; i++) {
-        blockElement[i].classList.add(`${targetClass}__block--closed`);
+      if (accordion && headerElement.length > 0 && blockElement.length > 0) {
+        for (let i = 0; i < headerElement.length; i++) {
+          headerElement[i].classList.remove(`${targetClass}__header--selected`);
+        }
+        for (let i = 0; i < blockElement.length; i++) {
+          blockElement[i].classList.add(`${targetClass}__block--closed`);
+        }
       }
     } catch (e) {
       reportError(e);
@@ -54,7 +59,11 @@ function renderAccordion(targetClass, maxViewport) {
   }
 
   function showBlock(currentBlock) {
-    currentBlock.classList.remove(`${targetClass}__block--closed`);
+    try {
+      currentBlock.classList.remove(`${targetClass}__block--closed`);
+    } catch (e) {
+      reportError(e);
+    }
   }
 }
 
