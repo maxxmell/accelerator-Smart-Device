806bf7a993907e5d3242ac11032cb3d5b28d6fa9
Sergey <Maxxmell@yandex.ru>
2022-10-25 10:05:59 +0300

    Bugfix

--- source/js/modules/dropdown.js
+++ source/js/modules/dropdown.js
@@ -1,5 +1,5 @@
 function renderDropdown(targetClass) {
-  let container = document.querySelector(`.${targetClass}`);
+  let container = document.querySelector(`.${targetClass}__container`);
   let button = document.querySelector(`.${targetClass}__button`);
   let hiddenBlock = document.querySelector(`.${targetClass}__block`);
   button.classList.remove(`${targetClass}__button--hidden`);
@@ -11,8 +11,8 @@ function renderDropdown(targetClass) {
 
   function toggleDropdown() {
     hiddenBlock.classList.toggle(`${targetClass}__block--hidden`);
-    container.classList.toggle(`${targetClass}--hidden`);
-    if (container.classList.contains(`${targetClass}--hidden`)) {
+    container.classList.toggle(`${targetClass}__container--hidden`);
+    if (container.classList.contains(`${targetClass}__container--hidden`)) {
       button.textContent = 'Подробнее';
     } else {
       button.textContent = 'Свернуть';
